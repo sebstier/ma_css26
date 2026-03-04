dc2664311c9b8a68811ee2fa0b03ac6b0c89fd97
Sebastian Stier <sebstier@Mac.fritz.box>
2026-03-04 12:48:10 +0100

    Built site for gh-pages

--- material/2_data_wrangling.R
+++ material/2_data_wrangling.R
@@ -12,23 +12,61 @@ library(tidyverse)
 list.files("data")
 df_trump <- read_csv("data/tweets_01-08-2021.csv",
                      col_types = "ccllcddTl")
+#df_trump <- read.csv("data/tweets_01-08-2021.csv")
+n_distinct(df_trump$id)
+nrow(df_trump)
 # df_trump <- read.csv("data/tweets_01-08-2021.csv", 
 #                      colClasses = c("id" = "character")) 
+# Check if the tweet IDs are unique
+n_distinct(df_trump$id)
+nchar(df_trump$id)
 summary(df_trump)
 glimpse(df_trump)
 
-# Check if the tweet IDs are unique
-n_distinct(df_trump$id)
-nrow(df_trump)
+# Check out the date range
+range(df_trump$date)
 
+# Create a code chunk where we add new variables to the data frame
+df_trump <- df_trump %>% 
+  mutate(day = as.Date(date),
+         year = year(date))
 
-# Use group_by() and summarize() to summarize the number of tweets per day 
+# Use mutate(), then group_by() and summarize() to summarize the number of tweets per day 
+df_trump %>% 
+  #mutate(day = as.Date(date)) %>% 
+  group_by(day) %>% 
+  summarise(n_tweets = n()) %>% 
+  arrange(desc(n_tweets))
 
-# Now also summarize the number of retweets
+# How many unique days are in the data frame?
+n_distinct(df_trump$day)
+
+# Now also summarize the number of retweets per day
+df_trump %>% 
+  group_by(day) %>% 
+  summarise(n_retweets = sum(retweets)) %>% 
+  arrange(desc(n_retweets))
 
 # Aggregate the number of tweets and retweets per year
 # Hint: use year()
+df_trump %>% 
+  group_by(year) %>% 
+  summarise(n_tweets = n(),
+            n_retweets = sum(retweets),
+            mean_rts = n_retweets / n_tweets)
 
+
+# We use mutate() and case_match() to create a variable indicating that the tweet was sent via 
+# iPhone or Android or another device
+table(df_trump$device)
+df_trump %>% 
+  count(device)
+df_trump <- df_trump %>% 
+  mutate(device_rec = case_match(device, 
+                                 "Twitter for Android" ~ "Android",
+                                 "Twitter for iPhone" ~ "iPhone",
+                                 .default = "Other"))
+df_trump %>% count(device_rec, device)
 
 # Some basic text operations ----
 
@@ -57,15 +95,9 @@ library(gapminder)
 # TODO HOMEWORK  
 # Calculate the (worldwide) average GDP per capita per year and plot this as a bar chart
 # Sum the total world population per year. Plot the results in a bar chart for the years 1992-2007
-
+case_match()
 
 # Visualizing the Trump tweets dataset ----
-
-# We use mutate() and case_match() to create a variable indicating that the tweet was sent via 
-#iPhone or Android or another device
-table(df_trump$device)
-df_trump %>% 
-  count(device)
 
 # Calculate the share of tweets per device that contain either "crazy" or "fake"
 
